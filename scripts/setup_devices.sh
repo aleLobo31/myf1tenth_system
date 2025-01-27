26b75606f7066a67760e29a075d779238818281a
JaimeGA <zocoblo1@gmail.com>
2025-01-27 10:50:41 +0100

    feat: imu rules added to setup_devices

--- scripts/setup_devices.sh
+++ scripts/setup_devices.sh
@@ -6,6 +6,10 @@ echo "SUBSYSTEM=="tty", ACTION=="add", ATTRS{idVendor}=="10c4", ATTRS{idProduct}
 touch /etc/udev/rules.d/99-vesc.rules
 echo "SUBSYSTEM=="tty", ACTION=="add", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", MODE="0666", GROUP="dialout", SYMLINK+="sensors/vesc"" | sudo tee /etc/udev/rules.d/99-vesc.rules > /dev/null
 
+# Creamos una norma para la IMU
+touch /etc/udev/rules.d/99-imu.rules
+echo "SUBSYSTEM=="tty", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", MODE="0666", GROUP="dialout", SYMLINK+="sensors/imu"" | sudo tee /etc/udev/rules.d/99-imu.rules > /dev/null
+
 # Cargamos las nuevas reglas y las aplicamos
 sudo udevadm control --reload-rules
 sudo udevadm trigger
