6d79f611cc95a68c46ac3c0b1e8511cc096a6eda
Jaime <zocoblo1@gmail.com>
2025-02-09 02:29:25 +0100

    fix: launch container's paths to packages fixed and name of reactive follower fixed

--- launch_container.sh
+++ launch_container.sh
@@ -6,8 +6,8 @@ docker run -it --privileged --net=host \
  --device /dev/sensors/vesc:/dev/sensors/vesc \
  --device /dev/sensors/imu:/dev/sensors/imu \
  --device /dev/input/js0:/dev/input/js0 \
- --volume=./f1tenth_stack:/root/f1tenth_ws/src/f1tenth_stack  \
- --volume=./manual_control:/root/f1tenth_ws/src/manual_control \
- --volume=./safety_pkg:/root/f1tenth_ws/src/safety_pkg \
- --volume=./reactive_follower_pkg:/root/f1tenth_ws/src/reactive_follower_pkg \
+ --volume=./src/f1tenth_stack:/root/f1tenth_ws/src/f1tenth_stack  \
+ --volume=./src/manual_control:/root/f1tenth_ws/src/manual_control \
+ --volume=./src/safety_pkg:/root/f1tenth_ws/src/safety_pkg \
+ --volume=./src/reactive_follower_pkg:/root/f1tenth_ws/src/reactive_follower_pkg \
  f1tenth-system
