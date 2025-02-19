d26570a9a245a91e5e61006ea8a1076d5342232d
Jaime <zocoblo1@gmail.com>
2025-02-20 00:35:12 +0100

    fix: packages inside the src folder

--- launch_container.sh
+++ launch_container.sh
@@ -1,13 +1,15 @@
 docker run -it --privileged --net=host \
- -v /var/run/dbus/:/var/run/dbus \
- -v /dev:/dev \
- -v /etc/udev/rules.d:/etc/udev/rules.d \
- --device /dev/sensors/lidar:/dev/sensors/lidar \
- --device /dev/sensors/vesc:/dev/sensors/vesc \
- --device /dev/sensors/imu:/dev/sensors/imu \
- --device /dev/input/js0:/dev/input/js0 \
- --volume=./src/f1tenth_stack:/root/f1tenth_ws/src/f1tenth_stack  \
- --volume=./src/manual_control_pkg:/root/f1tenth_ws/src/manual_control_pkg \
- --volume=./src/safety_pkg:/root/f1tenth_ws/src/safety_pkg \
- --volume=./src/reactive_follower_pkg:/root/f1tenth_ws/src/reactive_follower_pkg \
- f1tenth-system
+    -v /var/run/dbus/:/var/run/dbus \
+    -v /dev:/dev \
+    -v /etc/udev/rules.d:/etc/udev/rules.d \
+    --device /dev/sensors/lidar:/dev/sensors/lidar \
+    --device /dev/sensors/vesc:/dev/sensors/vesc \
+    --device /dev/sensors/imu:/dev/sensors/imu \
+    --device /dev/input/js0:/dev/input/js0 \
+    --env DISPLAY=$DISPLAY \
+    --volume /tmp/.X11-unix:/tmp/.X11-unix \
+    --volume=./src/f1tenth_stack:/root/f1tenth_ws/src/f1tenth_stack  \
+    --volume=./src/manual_control:/root/f1tenth_ws/src/manual_control \
+    --volume=./src/safety_pkg:/root/f1tenth_ws/src/safety_pkg \
+    --volume=./src/reactive_follower_pkg:/root/f1tenth_ws/src/reactive_follower_pkg \
+    f1tenth-system
