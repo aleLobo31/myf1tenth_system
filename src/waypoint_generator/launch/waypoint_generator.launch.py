f3295a42789abd16f203a7b35903c0ce099bad62
JaimeG-ELC <zocoblo@gmail.com>
2025-05-20 17:42:58 +0200

    feat: add waypoint generator launch file and parameters configuration

---
+++ src/waypoint_generator/launch/waypoint_generator.launch.py
@@ -0,0 +1,32 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    waypoint_generator_config = os.path.join(
+        get_package_share_directory('waypoint_generator_pkg'),
+        'config',
+        'waypoint_generator_params.yaml'
+    )
+
+    waypoint_generator_la = DeclareLaunchArgument(
+        'waypoint_generator_params',
+        default_value=waypoint_generator_config
+    )
+
+    waypoint_generator_node = Node(
+        package="waypoint_generator_pkg",
+        executable="waypoint_generator_node",
+        name="waypoint_generator_node",
+        parameters=[LaunchConfiguration('waypoint_generator_config')]
+    )
+
+    ld = LaunchDescription([waypoint_generator_la])
+
+    ld.add_action(waypoint_generator_node)
+
+    return ld
